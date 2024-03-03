af6edc71b0e8e0569ed7cc4dee2efa54d1f02016
fahimfaruque <fahim@frqpharmacy.com>
2024-03-02 21:46:15 -0500

    jj

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,9 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1, // Ensure only one slide is visible at a time
-    spaceBetween: 30, // Space between slides
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    // Responsive breakpoints
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20
-        },
-        // when window width is >= 480px
-        480: {
-            slidesPerView: 1,
-            spaceBetween: 30
-        },
-        // when window width is >= 640px
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 40
-        }
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: false,
+        freeMode: true, // This makes the slides "free" and not fixed to snap positions
+        // Removed navigation and pagination
+    });
 });
