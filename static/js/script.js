0c662958a0f735d4ca964f8901dc0b0687f7fdd4
fahimfaruque <fahim@frqpharmacy.com>
2024-03-02 21:01:56 -0500

    rest

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,15 @@
-var mySwiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If you want to add pagination
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1, // Display one slide at a time
+    spaceBetween: 30, // Add space between slides if needed
+    loop: false,
+    autoHeight: true, // Adjust height of swiper to match the content
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
+        clickable: true,
     },
-  
-    // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
-    // And if you need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-  
+});
