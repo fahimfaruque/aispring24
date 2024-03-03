18cca5316ffdd8a9800b4fffbb3656f92d89265b
fahimfaruque <fahim@frqpharmacy.com>
2024-03-02 19:57:15 -0500

    filter

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,22 @@
-var swiper = new Swiper('.mySwiper', {
-    pagination: {
-        el: '.swiper-pagination',
-    },
+var mySwiper = new Swiper('.swiper', {
+    // Optional parameters
     direction: 'horizontal',
     loop: true,
-});
+  
+    // If you want to add pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if you need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
