2903ef61150c2086dc688edd362b4c3ba4f3c7a2
fahimfaruque <fahim@frqpharmacy.com>
2024-03-02 21:22:14 -0500

    hyyh

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,7 @@
-// Initialize Swiper
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1, // Display one slide at a time
-    spaceBetween: 30, // Add space between slides if needed
-    loop: false,
-    autoHeight: true, // Adjust height of swiper to match the content
+    slidesPerView: 1, // Ensure only one slide is visible at a time
+    spaceBetween: 30, // Space between slides
+    loop: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -12,4 +10,22 @@ var swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 40
+        }
+    }
 });
